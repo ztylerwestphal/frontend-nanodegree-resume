7df0e92e8783109aaf8f1eaa90fcac86036f6826
Zach Westphal <ztylerwestphal@gmail.com>
2016-08-04 12:34:39 -0400

    Updated resumebuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,92 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  "name":"Zachary Westphal",
+  "role":"Front End Web Developer",
+  "contacts":{
+    "mobile":"410-440-1121",
+    "email":"ztylerwestphal@gmail.com",
+    "github":"https://github.com/ztylerwestphal",
+    "twitter":"@ztylerwestphal",
+    "location":"Baltimore, MD"
+  },
+  "welcome message":"Welcome to my resume.",
+  "skills":["javaScript",
+  "HTML",
+  "Angular.js",
+  "Meteor",
+  "MVC",
+  "CSS",
+  "jQuery"
+  ],
+  "biopic":"https://github.com/ztylerwestphal",
+  "display": function(){}
+};
+
+var education = {
+  "schools": [
+{
+  "name": "Helena High School",
+  "location": "Helena, Mt",
+  "degree": "Diploma",
+  "dates": 2001,
+  "url":"http://hhs.helenaschools.org/"
+},
+{
+  "name": "Community College of Baltimore County",
+  "location": "Essex, MD",
+  "degree": "Pursuing Associate's Degree",
+  "majors": ["Engineering"],
+  "dates":"Current Student",
+  "url":"http://www.ccbcmd.edu/"
+}
+  ],
+  "online courses":[
+{
+  "title": "Front End Nanodegree",
+  "school": "Udacity",
+  "dates": 2016,
+  "url": "http://www.udacity.com"
+}
+  ],
+  "display": function(){}
+};
+
+var work = {
+  "jobs": [
+{
+  "employer":"",
+  "title":"",
+  "location":"",
+  "dates":"",
+  "description":""
+},
+{
+  "employer":"",
+  "title":"",
+  "location":"",
+  "dates":"",
+  "description":""
+},
+{
+  "employer":"",
+  "title":"",
+  "location":"",
+  "dates":"",
+  "description":""
+}
+  ]
+};
+
+var projects = {
+  "projects":[
+{
+  "title":"",
+  "dates":"",
+  "description":"",
+  "images":[
+  "",
+  "",
+  "",
+  ]
+}
+  ]
+};
